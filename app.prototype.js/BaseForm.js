482927c17891a99775fc893db312df03ed57500a
ifsale <irony00100@gmail.com>
2019-03-20 07:29:00 +0700

    Update BaseForm.js

--- app.prototype.js/BaseForm.js
+++ app.prototype.js/BaseForm.js
@@ -1,121 +1,426 @@
-APP.Base.Form = APP.Base.Form || APP.Base.View.extend
-( {
-	id: 'BaseForm' ,
-	name: 'BaseForm' ,
-	xtype: 'form' ,
-	values: { } ,
-	handler: null ,
-	action: null ,
-	success: function( values ){ } ,
-	invalid: function( values , validator )
+var APP =
+{
+	id: 'APP' ,
+	name: 'APP' ,
+	env: 'develop' ,
+	Base: {} ,
+	Controllers: this.Controllers || {} ,
+	Forms: this.Forms || {} ,
+	Views: this.View || {} ,
+	Models: this.Models || {} ,
+	Elements: this.Elements || {} ,
+	Storage: this.Storage || {} ,
+	Config: this.Config || 
+	{
+		url: null ,
+		env: '/' ,
+		test_env: 0 ,
+		revision: '0.0.1'
+	} ,
+	Lang: this.Lang || {} ,
+	Storage: // this.Storage || // Seems to be present as native code
+	{
+		get: function( key , defaultVal )
+		{
+			/*if ( ( undefined === this._Data[ key ] || 
+					null === this._Data[ key ] ) && 
+					undefined !== this._Defaults[ key ] )
+			{
+				return this._Defaults[ key ];
+			}*/
+			if ( ( undefined === this._Data[ key ] ) && 
+					( typeof defaultVal !== 'undefined' ) )
+			{
+				return defaultVal;	
+			}
+			return this._Data[ key ];
+		} ,
+		set: function( key , value , persist )
+		{
+			// todo add persistent check
+			this._Data[ key ] = value;
+			if ( 1 == persist ){ this._Persist[ key ] = 1; }
+			return this;
+		} ,
+		all: function( )
+		{
+			return this._Data;
+		} ,
+		_Data: {} ,
+		_Persist: {}
+	} ,
+	Events: this.Events || 
+	{
+		add: function( event , el , fn , args )
+		{
+			fn = ( typeof fn === 'undefined' ) ? el[ event ] : fn;
+			element = ( el.hasOwnProperty( 'xtype' ) ) ? APP.getElById( el.id ) : el;
+			var handler = ( args ) ? 
+						fn.bind.apply( fn , [ el ].concat( args ) ) : fn.bind( el );
+			if ( element.addEventListener ) 
+			{
+				element.addEventListener( event , handler , true );
+			}
+			else if ( element.attachEvent ) 
+			{
+				element.attachEvent( 'on' + event , handler );
+			}
+		} ,
+		remove: function( event , el , fn )
+		{ 
+			fn = ( typeof fn === 'undefined' ) ? el[ event ] : fn;
+			element = ( el.hasOwnProperty( 'xtype' ) ) ? APP.getElById( el.id ) : el;
+			if ( element.removeEventListener ) 
+			{
+				element.removeEventListener( event , handler );
+			}
+			else if ( element.detachEvent ) 
+			{
+				element.detachEvent( 'on' + event , handler );
+			}
+		} ,
+		fire: function ( event , el )
+		{
+			element = ( el.hasOwnProperty( 'xtype' ) ) ? APP.getElById( el.id ) : el;
+			if ( element.fireEvent ) 
+			{
+				element.fireEvent( event );
+			}
+			else if ( element.dispatchEvent ) 
+			{
+				element.dispatchEvent( event );
+			}
+		} ,
+		async: function( fn , callback ) 
+		{
+			setTimeout( function( ) 
+			{
+				fn( );
+				if ( callback ){ callback( ); }
+			} , 0 );
+		}
+	} ,
+	getUrlParam: function ( name, url ) 
+	{
+		url = ( !url ) ? location.href : url;
+		name = name.replace( /[\[]/ ,"\\\[" ).replace( /[\]]/ ,"\\\]" );
+		var regexS = "[\\?&]" + name + "=([^&#]*)";
+		var regex = new RegExp( regexS );
+		var results = regex.exec( url );
+		return results == null ? null : results[ 1 ];
+	} ,
+	start: function( controller )
+	{
+		for ( var i in this._Exec )
+		{
+			if ( this._Exec[ i ].hasOwnProperty( 'attach' ) && 
+						typeof this._Exec[ i ].attach === 'string' )
+			{
+				this._Exec[ i ].addEvents( this._Exec[ i ].attach );
+			}
+			if ( this._Exec[ i ].hasOwnProperty( 'exec' ) && this._Exec[ i ].exec != null )
+			{
+				if ( typeof this._Exec[ i ].exec === 'string' )
+				{
+					this._Exec[ i ][ this._Exec[ i ].exec ]( );
+				}
+				else
+				{
+					this._Exec[ i ].exec( );
+				}
+			}
+		}
+		return this.Controllers[ controller ].run( ); 
+	} ,
+	getController: function( name )
 	{ 
-		this.log( [ 'form is invalid' , values , validator ] ); 
+		return this.Controllers[ name ]; 
 	} ,
-	getValues: function( )
+	getForm: function( name )
+	{ 
+		return this.Forms[ name ]; 
+	} ,
+	getView: function( name )
+	{ 
+		return this.Views[ name ]; 
+	} ,
+	getContainer: function( name )
+	{ 
+		return this.Elements[ name ]; 
+	} ,
+	getEl: function( selector )
 	{
-		this.values = { };
-		for ( var i in this.items )
+		el = document.querySelectorAll( selector );
+		return ( el.length > 1 ) ? el : el[ 0 ];
+	} ,
+	getElById: function( id )
+	{
+		return document.getElementById( id );
+	} ,
+	getElByClass: function( cls )
+	{
+		return document.getElementsByClassName( cls );
+	} ,
+	getElByName: function( name )
+	{
+		return document.getElementsByName( name );
+	} ,
+	getElByTag: function( tag )
+	{
+		return document.getElementsByTagName( tag );
+	} ,
+	isVisible: function( selector )
+	{
+		return ( 'none' === APP.getEl( selector ).style.display ) ? false : true;
+	} ,
+	addEl: function( tag )
+	{
+		return document.createElement( tag );
+	} ,
+	hide: function( el )
+	{
+		if ( el instanceof Array )
 		{
-			if ( 'function' === typeof this.items[ i ].getValue )
+			for ( var i in el)
 			{
-				this.values[ this.items[ i ].name ] = this.items[ i ].getValue( );
+				el[ i ].style.display = 'none';
 			}
-		}
-		return this.values;
-	} ,
-	setValues: function( values )
-	{
-		for ( var key in values )
-		{
-			this.values[ key ] = this.getItem( key ).setValue( values[ key ] );
-		}
-		return this;
-	} ,
-	reset: function( )
-	{
-		var value, xtype;
-		this.values = { };
-		for ( var i in this.items )
-		{
-			xtype = this.items[ i ].xtype;
-			switch ( xtype )
-			{
-				case 'checkboxgroup':
-					// todo
-				break;
-				case 'checkbox':
-					this.items[ i ].check( this.items[ i ].checked );
-				break;
-				case 'radio': // old code
-				case 'radiobutton':
-					this.items[ i ].reset( );
-					if ( this.items[ i ].checked.length > 0 )
-					{
-						this.items[ i ].check( this.items[ i ].checked , 1 );
-					}
-				break;
-				default:
-					this.items[ i ].setValue( this.items[ i ].defaultValue );
-			}
-			this.items[ i ].value = null;
-		}
-		APP.Validator.reset( this.items );
-		return this;
-	} ,
-	submit: function( )
-	{
-		APP.Events.async( function( )
-		{
-			this.listeners.beforeSubmit( this ); 
-		}.bind( this ) , function( )
-		{
-			APP.Events.async( function( )
-			{ 
-				this.validate( );
-			}.bind( this ) , function( )
-			{
-				this.listeners.afterSubmit( this );
-			}.bind( this ) );
-		}.bind( this ) );
-	} ,
-	validate: function( )
-	{
-		var validator = APP.Validator.validate( this.items , this.getValues( ) , this.name );
-		if ( validator.isValid )
-		{
-			this.success( this.values );
 		}
 		else
 		{
-			this.invalid( this.values , validator );
-		}
-		return false;
-	} ,
-	attachHandler: function( event )
-	{
-		event = ( typeof event === 'undefined' ) ? 'click' : event;
-		APP.Events.add( event , this.getItem( this.handler ) , function( event )
-		{
-			this.submit( );
-			//if ( !this.action )
-			//{
-				event.preventDefault( );
-				event.stopPropagation( );
-				return false;
-			//}
-		}.bind( this ) );
-		for ( var i in this.items )
-		{
-			if ( this.items[ i ].hasOwnProperty( 'fn' ) )
-			{
-				this.items[ i ].fn( );
-			}
+			el.style.display = 'none';
 		}
 		return this;
 	} ,
-	listeners:
+	show: function( el )
 	{
-		beforeSubmit: function( obj ){ } ,
-		afterSubmit: function( obj ){ }
+		if ( el instanceof Array )
+		{
+			for ( var i in el )
+			{
+				el[ i ].style.display = 'block';
+			}
+		}
+		else
+		{
+			el.style.display = 'block';
+		}
+		return this;
+	} ,
+	remove: function( el )
+	{
+		el.parentNode.removeChild( el );
+		return this;
+	} ,
+	render: function( el , html )
+	{
+		el.innerHTML = html;
+		return this;
+	} ,
+	hasClass: function( el , cls )
+	{
+		return ( el.classList.contains( cls ) ) ? true : false;
+	} ,
+	removeClass: function( el , cls )
+	{
+		return el.classList.remove( cls );
+	} ,
+	addClass: function( el , cls )
+	{
+		return el.classList.add( cls );
+	} ,
+	toggleClass: function( el , cls )
+	{
+		return el.classList.toggle( cls );
+	} ,
+	addEventsById: function( events , obj )
+	{
+		if ( null !== obj.getElById( ) )
+		{
+			var e = events.split( ',' );
+			for ( var a in e )
+			{
+				APP.Events.add( e[ a ] , obj );
+			}
+		}			
+	} ,
+	addEventsByClass: function( events , elements )
+	{
+		for ( var a = 0; a < elements.length; a++ )
+		{
+			var e = events.split( ',' );
+			for ( var i in e )
+			{
+				APP.Events.add( e[ i ] , elements[ a ] , elements[ e[ i ] ] );
+			}
+		}
+	} ,
+	extend: function( obj ) 
+	{
+		obj._extended = true;
+		return this._merge( this , obj , false );
+	} ,
+	log: function( )
+	{
+		// log function
+		if  ( 'develop' === this.env && typeof console != 'undefined' )
+		{
+			for ( var i = 0; i < arguments.length; i++ )
+			{
+				console.log( arguments[ i ] );
+			}
+		}
+	} ,
+	_Exec: this._Exec || {} ,
+	_create: function( obj )
+	{
+		switch ( obj.xtype )
+		{
+			case 'checkboxgroup':
+			case 'checkbox':
+				return this.Base.CheckBox.extend( obj );
+			break;
+			case 'radio':
+			case 'radiobutton':
+				return this.Base.RadioButton.extend( obj );
+			break;
+			case 'textfield':
+				return this.Base.TextField.extend( obj );
+			break;
+			case 'textarea':
+				return this.Base.TextArea.extend( obj );
+			break;
+			case 'element':
+				return this.Base.Element.extend( obj );
+			break;
+			case 'select':
+				return this.Base.Select.extend( obj );
+			break;
+			case 'button':
+				return this.Base.Button.extend( obj );
+			break;
+			case 'view':
+				return this.Base.View.extend( obj );
+			break;
+			case 'form':
+				return this.Base.Form.extend( obj );
+			break;
+			case 'model':
+				return this.Base.Model.extend( obj );
+			break;
+			break;
+			case 'tabs':
+				return this.Base.Tabs.extend( obj );
+			break;
+			case 'tabitem':
+				return this.Base.TabItem.extend( obj );
+			break;
+			//case 'controller': //controller is never an item
+			//	return this.Base.Controller.extend( obj );
+			//break;
+		}
+		return obj;
+	} ,
+	_merge: function( cls , obj , clone )
+	{
+		var cl = ( clone ) ? this._clone( cls ) : cls;
+		for ( var i in obj )
+		{ 
+			if ( obj[ i ] instanceof Array || typeof obj[ i ] === 'object' )
+			{
+				if ( !cl.hasOwnProperty( i ) )
+				{
+					cl[ i ] = ( obj[ i ] instanceof Array ) ? [] : {};
+				}
+				for ( var a in obj[ i ] )
+				{
+					if ( 'items' !== i )
+					{
+						cl[ i ][ a ] = obj[ i ][ a ];  
+					}
+				}
+			}
+			else
+			{ 
+				cl[ i ] = obj[ i ]; 
+			}
+			if ( obj.hasOwnProperty( 'items' ) )
+			{
+				for ( var b in obj.items )
+				{
+					if ( 'items' === i && 
+						!obj.items[ b ].hasOwnProperty( '_extended' ) && 
+									obj.items[ b ].hasOwnProperty( 'xtype' ) )
+					{
+						obj.items[ b ] = this._create( obj.items[ b ] );
+					}
+					cl.items[ b ] = obj.items[ b ];  
+					obj.items[ b ]._parent = cl;
+				}
+			}
+			if ( ( cl.hasOwnProperty( 'attach' ) && typeof cl.attach === 'string' || 
+							cl.hasOwnProperty( 'exec' ) && cl.exec != null ) && 
+										!this._Exec.hasOwnProperty( cl.name ) )
+			{
+				this._Exec[ cl.name ] = cl;
+			}
+		}
+		if ( cl.hasOwnProperty( 'xtype' ) )
+		{
+			this._xtype( cl );
+		}
+		return cl;
+	} ,
+	_clone: function( obj ) 
+	{
+		if ( obj == null || typeof( obj ) != 'object' )
+		{
+			return obj;
+		}
+		var temp = obj.constructor( );
+		for ( var key in obj ) 
+		{
+			if ( obj.hasOwnProperty( key ) ) 
+			{
+				temp[ key ] = this._clone( obj[ key ] );
+			}
+		}
+		return temp;
+	} ,
+	_xtype: function( cl )
+	{
+		switch( cl.xtype )
+		{
+			case 'form': 
+				if ( cl.name !== 'BaseForm' )
+				{
+					this.Forms[ cl.name ] = cl;
+				}
+			break;
+			case 'view': 
+				if ( cl.name !== 'BaseView'  )
+				{
+					this.Views[ cl.name ] = cl;
+				}
+			break;
+			case 'controller': 
+				if ( cl.name !== 'BaseController'  )
+				{
+					this.Controllers[ cl.name ] = cl;
+				}
+			break;
+			case 'model':
+				if ( cl.name !== 'BaseModel'  )
+				{
+					this.Models[ cl.name ] = cl;
+				}
+			break;
+			case 'element':
+				if ( cl.name !== 'BaseElement'  )
+				{
+					this.Elements[ cl.name ] = cl;
+				}
+			break;
+		}
 	}
-} );
+};
