45e2d6fc9ee8285ede1a333762d5b0bb7bde54a4
ifsale <irony00100@gmail.com>
2021-02-15 20:40:56 +0700

    Update MetaTags.php

--- models/MetaTags.php
+++ models/MetaTags.php
@@ -19,20 +19,33 @@
 			$this->_data = $data;
 		}
 		
-		public function __call( $method , $args = array( ) )
+		public function val( $key )
 		{
-			if ( 0 === strpos( $method , 'get_' ) )
+			$this->_build( );
+			if ( empty( $this->_data ) ){ return null; }	
+			return $this->_data[ $key ];
+		}
+		
+		public function tag( $key )
+		{
+			$this->_build( );
+			if ( empty( $this->_data ) ){ return null; }		
+			$value = preg_replace( '/\s+/' , ' ' , $this->_data[ $field ] );
+			return $this->_buildTag( $key , $value );
+		}
+		
+		public function all( )
+		{
+			$this->_build( );
+			if ( empty( $this->_data ) ){ return null; }		
+			$markup = null;
+			foreach ( $this->_data as $k => $v )
 			{
-				if ( empty( $this->_data ) ){ return null; }
-				if ( !$this->_data[ $field ] )
-				{
-					return null;
-				}
-				$field = substr( $method , 4 );
-				$value = preg_replace( '/\s+/' , ' ' , $this->_data[ $field ] );
-				return "\t" . '<meta name="' . $field . '" content="' . trim( $value ) . '">' . "\n";
+				if ( !$v ){ continue; }
+				$v= preg_replace( '/\s+/' , ' ' , $v );
+				$markup .= $this->_buildTag( $k , $v );
 			}
-			return call_user_func_array( array( $this , $method ), $args );
+			return $markup;
 		}
 		
 		public function render( )
@@ -102,10 +115,69 @@
 			}
 			return $data;
 		}
-		
+		/**
+		*
+		*/	
 		protected $_data = array( );
-		
+		/**
+		*
+		*/	
 		protected $_pageID = null;
-		
+		/**
+		*
+		*/	
 		protected $_callback = null;
+		/**
+		*
+		*/	
+		protected $_isBuilt = false;
+		/**
+		*
+		*/	
+		protected function _buildTag( $tag , $value )
+		{
+			if ( 'title' === $tag )
+			{
+				return "\t" . '<title>' . trim( $value ) . '</title>' . "\n";
+			}
+			else if ( 0 === strpos( strtolower( $tag ) , 'og:' ) )
+			{
+				return "\t" . '<meta property="' . $tag . '" content="' . trim( $value ) . '" />' . "\n";
+			}
+			else
+			{
+				return "\t" . '<meta name="' . $tag . '" content="' . trim( $value ) . '" />' . "\n";
+			}
+		}
+		
+		public function __call( $method , $args = array( ) )
+		{
+			if ( 0 === strpos( $method , 'get_' ) )
+			{
+				if ( empty( $this->_data ) ){ return null; }
+				if ( !$this->_data[ $field ] )
+				{
+					return null;
+				}
+				$field = substr( $method , 4 );
+				$value = preg_replace( '/\s+/' , ' ' , $this->_data[ $field ] );
+				return "\t" . '<meta name="' . $field . '" content="' . trim( $value ) . '">' . "\n";
+			}
+			return call_user_func_array( array( $this , $method ), $args );
+		}
+		
+		protected function _build( )
+		{
+			if ( $this->_isBuilt ){ return; }
+			$params = array( $this->_pageID , &$this->_data ); 
+			ptc_log( $params , 'Website metatags are been compiled!' , 
+					\App::option( 'website.debug_category' ) . ' Action' );
+			if ( $this->_callback )
+			{
+				ptc_log( $params , 'Website metatags is beeing fired!' , 
+						\App::option( 'website.debug_category' ) . ' Action' );
+				\Event::fire( 'metatags.callback' , $params );
+			}
+			$this->_isBuilt = true;
+		}
 	}
